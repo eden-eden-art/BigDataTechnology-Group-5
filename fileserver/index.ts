74acc7d7a7a1e250be0d41dd43f660fe835255a6
RazorSh4rk <iamrazorshark@gmail.com>
2024-09-19 17:29:59 -0500

    file server impl

---
+++ fileserver/index.ts
@@ -0,0 +1,37 @@
+import express from "express";
+import cors from "cors";
+import fs from "node:fs/promises";
+import filebrowser from "serve-handler";
+import zip from "zip-local";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+fs.mkdir("data/")
+    .catch(err => { });
+
+app.post("/", (req, res) => {
+    const { file, content } = req.body;
+    console.log(`saving file: [${file}] - [${content}]`);
+    fs.writeFile(`data/${file}`, content);
+
+    res.status(200).send();
+});
+
+app.get("/", (req, res) => {
+    console.log(`serving file browser`);
+    return filebrowser(req, res, { "public": "data/" });
+});
+
+app.get("/download", async (req, res) => {
+    console.log(`zipping files`);
+
+    zip.sync.zip("./data").compress().save("./alldata.zip")
+
+    res.sendFile(`${__dirname}/alldata.zip`)
+});
+
+app.listen(3000, () => {
+    console.log("server started");
+});
---
+++ fileserver/run.sh
@@ -0,0 +1,1 @@
+docker run -v /home/razor/college/CS523/project/fileserver/data:/app/data -p 3000:3000 fileserver
